b82c357b406ac2d209121f72147df3e615cc5740
ShadowAdi <shadowshukla76@gmail.com>
2026-02-09 02:49:40 +0530

    fix button type bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className="antialiased">
         <AuthProvider>
           {children}
-          <Toaster />
+          <Toaster richColors />
         </AuthProvider>
       </body>
     </html>
