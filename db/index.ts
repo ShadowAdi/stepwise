879960188be476a3c386d9445f6c53c8d0cab046
ShadowAdi <shadowshukla76@gmail.com>
2026-02-08 22:12:42 +0530

    fix issue

--- db/index.ts
+++ db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
 
 
-const connectionString = process.env.DATABASE_URL
+export const connectionString = process.env.DATABASE_URL
 
 const client = postgres(connectionString!, { prepare: false })
 export const db = drizzle(client)
