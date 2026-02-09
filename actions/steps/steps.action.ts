a6510d01f6e0725f415c02a77ab045562c9286ff
ShadowAdi <shadowshukla76@gmail.com>
2026-02-10 01:12:00 +0530

    added create step

---
+++ actions/steps/steps.action.ts
@@ -0,0 +1,83 @@
+import { getUserIdFromToken } from "@/helper/get-user-from-id";
+import { ActionResponse, CreateStepDTO, StepResponse } from "@/types";
+import { getDemoById } from "../demos/demos.action";
+import { db } from "@/db";
+import { steps } from "@/db/schema";
+
+export const createStep = async (
+    payload: CreateStepDTO,
+    token: string,
+    demoId: string
+): Promise<ActionResponse<StepResponse>> => {
+    try {
+        if (!token) {
+            return {
+                success: false,
+                error: "Authentication required"
+            };
+        }
+
+        if (!demoId) {
+            return {
+                success: false,
+                error: "Demo Id is required"
+            };
+        }
+
+        const authResult = await getUserIdFromToken(token);
+        if (!authResult.success) {
+            return {
+                success: false,
+                error: authResult.error
+            };
+        }
+
+        const isDemoExist = await getDemoById(demoId, token)
+
+        if (!isDemoExist) {
+            return {
+                success: false,
+                error: `Demo not found for the given id`
+            };
+        }
+
+        if (!payload.title) {
+            return {
+                success: false,
+                error: "Title is required"
+            };
+        }
+
+
+        const [step] = await db.insert(steps).values({
+            title: payload.title,
+            demoId: demoId,
+            imageUrl: payload.imageUrl,
+            position: payload.position,
+            description: payload.description,
+        }).returning();
+
+        return {
+            success: true,
+            data: {
+                ...step,
+            }
+        };
+    } catch (error) {
+        console.error("Failed to create step:", error);
+
+        if (error instanceof Error) {
+            if (error.message.includes("connection")) {
+                return {
+                    success: false,
+                    error: "Database connection failed. Please try again later"
+                };
+            }
+        }
+
+        return {
+            success: false,
+            error: "Failed to create step. Please try again"
+        };
+    }
+};
