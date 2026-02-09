3b52a682ff3740655c6d1f30ef4333a801ff7af2
ShadowAdi <shadowshukla76@gmail.com>
2026-02-09 21:27:49 +0530

    quick fix

--- app/(protected)/dashboard/[id]/steps/page.tsx
+++ app/(protected)/dashboard/[id]/steps/page.tsx
@@ -1,14 +1,13 @@
 "use client"
 
 import { useParams } from 'next/navigation';
-import React from 'react'
 
 const page = () => {
   const params = useParams();
   const demoId = params.id as string;
 
   return (
-    <div>page</div>
+    <main >page</main>
   )
 }
 
